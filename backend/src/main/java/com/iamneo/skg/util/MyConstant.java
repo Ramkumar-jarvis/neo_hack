8dda74dd0f71a441cf52b36e0cf431041d897e73
RamTheGreatDeveloper <ramkumars.trichy@gmail.com>
2023-09-16 14:08:17 +0530

    Revert "Revert "login and trainer api""
    This reverts commit 6c45f7932881b6dae70de3655abd27cdbb5dbf6a.

--- backend/src/main/java/com/iamneo/skg/util/MyConstant.java
+++ backend/src/main/java/com/iamneo/skg/util/MyConstant.java
@@ -4,7 +4,7 @@ public class MyConstant {
     public static final String ADMIN_API_PATH = "/api/v1/admin";
     public static final String TRAINER_API_PATH = "/api/v1/trainer";
     public static final String STUDENT_API_PATH = "/api/v1/student";
-    public static final String REACT_URL = "http://localhost:3000";
+    public static final String REACT_URL = "http://localhost:5173";
     public static final String AUTH_API_PATH = "/api/v1/auth";
     public static final String DEFAULT_URL = "http://localhost:8181";
     public static final String SWAGGER_UI = "/swagger-ui";
